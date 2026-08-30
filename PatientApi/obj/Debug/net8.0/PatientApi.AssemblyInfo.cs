79c16b9bcd258d9af514747ba078aaa720db41bb
Marcello Legiser <mlegister626@gmail.com>
2026-08-30 15:19:47 -0400

    Updating numerous DLL files.

--- PatientApi/obj/Debug/net8.0/PatientApi.AssemblyInfo.cs
+++ PatientApi/obj/Debug/net8.0/PatientApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a6f1bab1e5294b4e12591deb1591c479c33a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c9f3c98a41940d5d2c6cee30dd406797f20c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
