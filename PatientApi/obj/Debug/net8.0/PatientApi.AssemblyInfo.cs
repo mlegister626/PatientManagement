210d3bf157642844a8bc7b184ee000abe33043d2
Marcello Legiser <mlegister626@gmail.com>
2026-09-13 11:34:54 -0400

    Publishing to GitHub. Asseblies need updated.

--- PatientApi/obj/Debug/net8.0/PatientApi.AssemblyInfo.cs
+++ PatientApi/obj/Debug/net8.0/PatientApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42053ede7cf88ee635e09d941ee2c11c3177387c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95fde81db13e2f9e835cb47395747aca0ad280ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
